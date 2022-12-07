a3c28c922e7556d02ab717a00a3e17ad04c507aa
sarahannata <sarahannata@gmail.com>
2022-12-07 15:54:52 +0700

    Membuat public void display

--- Exe4_139/Program.cs
+++ Exe4_139/Program.cs
@@ -40,5 +40,19 @@ namespace Exercise_4
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next;
         }
+        public void display()
+        {
+            Node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();    
+            }
+        }
     }
 }
